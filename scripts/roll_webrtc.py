75d7cc61c6ea6b14ed491bb1b279a4d7ca76e466
Jianjun Zhu <jianjun.zhu@intel.com>
2021-04-12 16:35:39 +0800

    Update auto roller.

--- scripts/roll_webrtc.py
+++ scripts/roll_webrtc.py
@@ -3,12 +3,20 @@
 # SPDX-License-Identifier: Apache-2.0
 
 '''Script to roll owt-deps-webrtc revision.
+
+This script is expected to run by GitHub action runners when a new change is
+committed to owt-deps-webrtc. It updates the revision in DEPS to use the latest
+owt-deps-webrtc. A change will be submitted to owt-client-native's
+auto-rollers/webrtc branch. And a pull request will be opened if it doesn't
+exist.
 '''
 
 import os
 import sys
 import argparse
 import re
+import subprocess
+import requests
 
 SRC_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DEPS_PATH = os.path.join(SRC_PATH, 'DEPS')
@@ -34,10 +42,25 @@ def roll(revision):
     return
 
 
-def commit(old_revision, new_revision):
+def commit_message(old_revision, new_revision):
     message = 'Roll WebRTC revision %s..%s.' % (
         old_revision[:8], new_revision[:8])
+    return message
+
+
+def commit(old_revision, new_revision):
+    message = commit_message(old_revision, new_revision)
     # Create a git commit with message above.
+    subprocess.call(['git', 'add', 'DEPS'], cwd=SRC_PATH)
+    subprocess.call(['git', 'commit', '-m', message], cwd=SRC_PATH)
+    # Force push because when a new version of owt-deps-webrtc is available, the old commit could be rewritten.
+    subprocess.call(['git', 'push', '-f', 'https://github.com/open-webrtc-toolkit/owt-client-native.git',
+                     'HEAD:refs/heads/auto-rollers/webrtc'])
+
+
+def pr(old_revision, new_revision):
+    '''Create a pull request. If a PR is already open for webrtc roller, do nothing.'''
+    # Check if a pull request exists.
 
 
 def main():
